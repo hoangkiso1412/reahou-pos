1fce89cbd557609a10369b0634b7698c394fa93a
homsrieng <srieng.planb@gmail.com>
2019-07-21 11:45:13 +0700

    test git push

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -18,7 +18,7 @@
 <div class="row">
     <div class="col-xl-3 col-lg-6 col-12">
         <div class="card">
-            <div class="card-content">
+            <div class="card-content hidden">
                 <div class="media align-items-stretch">
                     <div class="p-2 text-center bg-primary bg-darken-2">
                         <i class="fa fa-file-text-o text-bold-200  font-large-2 white"></i>
