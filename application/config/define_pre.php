fa730b180f45e1b5b380633e491119bc8fc19de0
limengkruy <kruylimeng@yahoo.com>
2019-08-07 13:44:36 +0700

    license update

--- application/config/define_pre.php
+++ application/config/define_pre.php
@@ -1,4 +1,4 @@
 <?php
-define('SERVICE', 'http://reahou.com/license/check');
+define('SERVICE', 'http://security.reahou.com/check');
 define('APPVER', '?v=4.3');
 define('UPDATE_SERVICE', 'http://provider.ultimatekode.com/updates/');// change it to on for exclusive
