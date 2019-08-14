d1479146122f0b2c40c4f05a1ac949ce865338ed
homsrieng <srieng.planb@gmail.com>
2019-08-14 22:32:29 +0700

    update deign report customer credit group by name and get data from server

---
+++ application/views/customers/customercreditbyname.php
@@ -0,0 +1,161 @@
+<div class="content-body">
+    <div class="card">
+        <div class="card-header">
+            <h4 class="card-title"><?php echo "Customer Credit Summary"//$this->lang->line('Manage Invoices') ?></h4>
+            <a class="heading-elements-toggle"><i class="fa fa-ellipsis-v font-medium-3"></i></a>
+            <div class="heading-elements">
+                <ul class="list-inline mb-0">
+                    <li><a data-action="collapse"><i class="ft-minus"></i></a></li>
+                    <li><a data-action="expand"><i class="ft-maximize"></i></a></li>
+                    <li><a data-action="close"><i class="ft-x"></i></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="card-content">
+            <div id="notify" class="alert alert-success" style="display:none;">
+                <a href="#" class="close" data-dismiss="alert">&times;</a>
+
+                <div class="message"></div>
+            </div>
+            <div class="card-body">
+                <table id="invoices" class="table table-striped table-bordered zero-configuration ">
+                    <thead>
+                    <tr>
+                        <th><?php echo $this->lang->line('No') ?></th>
+                        <th><?php echo "Account Name"//echo $this->lang->line('Customer') ?></th>
+                        <th><?php echo $this->lang->line('Amount') ?></th>
+                        <th><?php echo "Paid"//$this->lang->line('Amount') ?></th>
+                        <th><?php echo "Debit" ?></th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    </tbody>
+                    <tfoot>
+                    <tr>
+                        <th><?php echo $this->lang->line('No') ?></th>
+                        <th><?php echo "Account Name"//echo $this->lang->line('Customer') ?></th>
+                        <th><?php echo $this->lang->line('Amount') ?></th>
+                        <th><?php echo "Paid"//$this->lang->line('Amount') ?></th>
+                        <th><?php echo "Debit" ?></th>
+                    </tr>
+                    </tfoot>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div id="delete_model" class="modal fade">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+
+                <h4 class="modal-title"><?php echo $this->lang->line('Delete Invoice') ?></h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                            aria-hidden="true">&times;</span></button>
+            </div>
+            <div class="modal-body">
+                <p><?php echo $this->lang->line('delete this invoice') ?> ?</p>
+            </div>
+            <div class="modal-footer">
+                <input type="hidden" id="object-id" value="">
+                <input type="hidden" id="action-url" value="invoices/delete_i">
+                <button type="button" data-dismiss="modal" class="btn btn-primary"
+                        id="delete-confirm"><?php echo $this->lang->line('Delete') ?></button>
+                <button type="button" data-dismiss="modal"
+                        class="btn"><?php echo $this->lang->line('Cancel') ?></button>
+            </div>
+        </div>
+    </div>
+</div>
+<script type="text/javascript">
+    $(document).ready(function () {
+        draw_data();
+
+        function draw_data(start_date = '', end_date = '') {
+            $('#invoices').DataTable({
+                'processing': true,
+                'serverSide': true,
+                'stateSave': true,
+                responsive: true,
+                'order': [],
+                'ajax': {
+                    'url': "<?php echo site_url('customercredit/ajax_list_group')?>",
+                    'type': 'POST',
+                    'data': {
+                        '<?=$this->security->get_csrf_token_name()?>': crsf_hash,
+                        start_date: start_date,
+                        end_date: end_date
+                    }
+                },
+                'columnDefs': [
+                    {
+                        'targets': [0],
+                        'orderable': false,
+                    },
+                ],
+                "drawCallback": function ( settings ) {
+                    var api = this.api();
+                    var rows = api.rows( {page:'current'} ).nodes();
+                    var last=null;
+                    var subTotal = new Array();
+                    var groupID = -1;
+                    var aData = new Array();
+                    var index = 0;
+                    
+                    api.column(1, {page:'current'} ).data().each( function ( group, i ) {
+                        
+                    // console.log(group+">>>"+i);
+                    
+                    var vals = api.row(api.row($(rows).eq(i)).index()).data();
+                    var salary = vals[7] ? parseFloat(vals[7]) : 0;
+
+                    // alert(salary);
+                    
+                    if (typeof aData[group] == 'undefined') {
+                        aData[group] = new Array();
+                        aData[group].rows = [];
+                        aData[group].salary = [];
+                    }
+                
+                        aData[group].rows.push(i); 
+                        aData[group].salary.push(salary); 
+                        
+                    } );
+                    var idx= 0;
+                    for(var office in aData){
+                        idx =  Math.max.apply(Math,aData[office].rows);
+                        var sum = 0; 
+                        $.each(aData[office].salary,function(k,v){
+                            sum = sum + v;
+                        });
+                        console.log(aData[office].salary);
+                    };
+                },
+                dom: 'Blfrtip',
+                buttons: [
+                    {
+                        extend: 'excelHtml5',
+                        footer: true,
+                        exportOptions: {
+                            columns: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+                        }
+                    }
+                ],
+            });
+        };
+
+        $('#search').click(function () {
+           
+            var start_date = $('#start_date').val();
+            var end_date = $('#end_date').val();
+            if (start_date != '' && end_date != '') {
+                $('#invoices').DataTable().destroy();
+                draw_data(start_date, end_date);
+            } else {
+                alert("Date range is Required");
+            }
+        });
+    });
+</script>
